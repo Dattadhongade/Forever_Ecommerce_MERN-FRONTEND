4032a49d3fe94b00bca2be278ae4315d30f473f0
Datta Dhongade <dhongadedattatray12@gmail.com>
2026-02-05 22:42:41 +0530

    first commit

---
+++ src/context/ShopContext.jsx
@@ -0,0 +1,25 @@
+// Create context
+import { createContext, useState } from "react";
+import { products } from "../assets/assets";
+export const shopContext = createContext();
+
+// Provide context (provider)
+const shopContextProvider = (props) => {
+  const currency = "$";
+  const delivery_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+  const value = {
+    products,
+    currency,
+    delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+  };
+  return (
+    <shopContext.Provider value={value}>{props.children}</shopContext.Provider>
+  );
+};
+export default shopContextProvider;
